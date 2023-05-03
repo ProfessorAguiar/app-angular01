14dfc0bb471a5033d31d94b37c4ac0acfeadb0f3
Vinicius Aguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-05-03 18:39:26 -0300

    Primeiro Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TituloComponent } from './components/titulo/titulo.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BarraNavegacaoComponent } from './components/barra-navegacao/barra-navegacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent,
+    BarraNavegacaoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/barra-navegacao/barra-navegacao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barra-navegacao',
+  templateUrl: './barra-navegacao.component.html',
+  styleUrls: ['./barra-navegacao.component.css']
+})
+export class BarraNavegacaoComponent {
+
+}
+""
