138e44965b11af281e85846e02d1f6a41f7e19f4
Vinicius Aguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-05-03 21:39:55 -0300

    03-05

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { TituloComponent } from './components/titulo/titulo.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BarraNavegacaoComponent } from './components/barra-navegacao/barra-navegacao.component';
+import { ConteudoComponent } from './components/conteudo/conteudo.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TituloComponent,
-    BarraNavegacaoComponent
+    BarraNavegacaoComponent,
+    ConteudoComponent,
+    FooterComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
